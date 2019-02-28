796edd4486ac010d05886a5e9d8c1fc965e9653d
Mikey <42908179+21Mikey@users.noreply.github.com>
2019-02-28 09:50:12 +0530

    Add files via upload
    master

---
+++ Static-addition.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>         // Static Program
+int main()                 // addition of Two Numbers.
+{
+	int a=10,b=23,c;
+	 
+	  c = a + b;
+	  printf("%d",c);
+      
+	  return 0;	
+}
---
+++ User-Input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+   {
+   	int a;
+   	
+   printf("Enter the value:");
+   scanf("%d",&a);
+   	 
+   	 printf("\nThe output of the value:%d",a);
+   	
+   	return 0;
+   }
